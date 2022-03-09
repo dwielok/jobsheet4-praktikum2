a7a915279a4c230762cbbe63f3cff6ae70eb3003
dwielok <dwieloknuraini773@gmail.com>
2022-03-09 23:38:11 +0700

    bootstrap dari  jobsheet 3

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+Route::get('/', [PageController::class, 'index']);
+
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/testmonial', [PageController::class, 'testmonial']);
+
+Route::get('/clients', [PageController::class, 'clients']);
+
+Route::get('/contact', [PageController::class, 'contact']);
